25a321b321a0c6f6aa25b9c27a2c613ff8bd6af3
huangzhiyong <75917702+ipromise24@users.noreply.github.com>
2025-10-28 16:52:12 +0800

    feat(birthday): change text

--- js/verification.js
+++ js/verification.js
@@ -1,8 +1,8 @@
 $('#login-button').click(function (event) {
     let userName = document.getElementById("userName").value;
     let pwd = document.getElementById("pwd").value;
-    if (userName == "chensi" && pwd == "1030") {
-        $('#h').text("欢迎回来！");
+    if (userName == "陈思" && pwd == "1030") {
+        $('#h').text("欢迎你回来！");
         event.preventDefault();
         $('form').fadeOut(500);
         $('.wrapper').addClass('form-success');
